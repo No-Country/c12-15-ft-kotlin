2a219d30397e441aa5fc89e7b00d0f14edd65a3a
Clarke Lamothe <clarkelamothe@yahoo.fr>
2023-07-06 06:58:23 -0300

    onboarding fragment created

---
+++ app/src/main/java/com/nocountry/movie_no_country/OnboardingFragment.kt
@@ -0,0 +1,29 @@
+package com.nocountry.movie_no_country
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nocountry.movie_no_country.databinding.FragmentOnboardingBinding
+
+class OnboardingFragment : Fragment() {
+    private var binding: FragmentOnboardingBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
