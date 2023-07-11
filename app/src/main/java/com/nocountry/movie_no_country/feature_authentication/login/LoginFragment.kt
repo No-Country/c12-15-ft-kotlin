51904250e87077cea4903a4a3a69256a31ec8e64
SantiagoG <santigime972@gmail.com>
2023-07-11 17:02:51 -0300

    added navigation and button toggle

--- app/src/main/java/com/nocountry/movie_no_country/feature_authentication/login/LoginFragment.kt
+++ app/src/main/java/com/nocountry/movie_no_country/feature_authentication/login/LoginFragment.kt
@@ -5,14 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.nocountry.movie_no_country.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
-    private var _binding : FragmentLoginBinding? = null
-    private val binding get() =  _binding!!
-
+    private var binding : FragmentLoginBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,8 +22,18 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentLoginBinding.inflate(inflater,container,false)
-        return binding.root
+        binding = FragmentLoginBinding.inflate(inflater,container,false)
+        binding?.buttonLogin2?.setOnClickListener {
+        val action = LoginFragmentDirections.actionFragmentLoginToHomeFragment()
+            findNavController().navigate(action)
+        }
+        return binding?.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+
     }
 
 }
