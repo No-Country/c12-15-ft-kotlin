c66cf0a90ae1727d73b15dc2e6caa7340ce063cd
Clarke Lamothe <clarkelamothe@yahoo.fr>
2023-07-15 04:28:52 -0300

    api result model added

--- app/src/main/java/com/nocountry/movie_no_country/feature_home/data/network/MovieApi.kt
+++ app/src/main/java/com/nocountry/movie_no_country/feature_home/data/network/MovieApi.kt
@@ -1,5 +1,6 @@
 package com.nocountry.movie_no_country.feature_home.data.network
 
+import com.nocountry.movie_no_country.core.data.model.ApiResult
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -10,5 +11,5 @@ interface MovieApi {
     suspend fun getPopularMovies(
         @Query("language") language: String,
         @Query("region") region: String
-    ): Response<MovieDto>
+    ): Response<ApiResult<MovieDto>>
 }
