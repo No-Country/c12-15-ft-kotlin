487ada2dcffb8e9ce57a2855a05fe2fb7e728a02
SantiagoG <santigime972@gmail.com>
2023-07-28 02:23:18 -0300

    added progress bard

--- app/src/main/java/com/nocountry/movie_no_country/feature_search/SearchFragment.kt
+++ app/src/main/java/com/nocountry/movie_no_country/feature_search/SearchFragment.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SearchView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.nocountry.movie_no_country.databinding.FragmentSearchBinding
 import com.nocountry.movie_no_country.feature_search.presentation.SearchAdapter
 import com.nocountry.movie_no_country.feature_search.presentation.SearchViewModel
+import com.nocountry.movie_no_country.feature_search.presentation.UiState
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -28,34 +28,39 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentSearchBinding.inflate(inflater,container,false)
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
         setupObserver()
         setListener()
         return binding?.root
     }
 
-    private fun setupObserver(){
+    private fun setupObserver() {
         viewLifecycleOwner.lifecycleScope.launch {
-            viewModel.search.observe(viewLifecycleOwner){
-                binding?.includeListMovie?.apply {
-                    if (it != null) {
-                        searchAdapter = SearchAdapter(it)
+            viewModel.search.observe(viewLifecycleOwner) {
+                when (it) {
+                    is UiState.Error -> binding?.loadingSearch?.progressBar?.isVisible = false
+                    UiState.Loading -> binding?.loadingSearch?.progressBar?.isVisible = true
+                    is UiState.Success -> binding?.includeListMovie?.apply {
+                        binding?.loadingSearch?.progressBar?.isVisible = false
+                        searchAdapter = SearchAdapter(it.items)
+                        rvSearch.adapter = searchAdapter
                     }
-                    rvSearch.adapter = searchAdapter
                 }
+
             }
         }
     }
-    private fun setListener(){
-        binding?.searchView?.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String?)= false
+
+    private fun setListener() {
+        binding?.searchView?.setOnQueryTextListener(object :
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?) = false
 
             override fun onQueryTextChange(newText: String?): Boolean {
                 newText?.let { viewModel.getSearch(it) }
                 binding?.textviewGone?.isVisible = newText?.isBlank() == true
                 return true
             }
-
         })
     }
 
--- app/src/main/java/com/nocountry/movie_no_country/feature_search/presentation/SearchViewModel.kt
+++ app/src/main/java/com/nocountry/movie_no_country/feature_search/presentation/SearchViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nocountry.movie_no_country.core.data.model.NetworkResult
 import com.nocountry.movie_no_country.feature_home.domain.usecase.BuildPosterUrlUseCase
+import com.nocountry.movie_no_country.feature_home.presentation.model.HomeRecyclerItem
 import com.nocountry.movie_no_country.feature_search.domain.usecase.GetSearchUseCase
 import kotlinx.coroutines.launch
 
@@ -14,18 +15,27 @@ class SearchViewModel(
     private val getSearchs : GetSearchUseCase,
     private val buildPosterUrlUseCase: BuildPosterUrlUseCase
 ):ViewModel() {
-    private val _search = MutableLiveData<List<String>>()
-    var search : LiveData<List<String>> = _search
+    private val _search = MutableLiveData<UiState>(UiState.Loading)
+    var search : LiveData<UiState> = _search
 
     fun getSearch(query:String){
         viewModelScope.launch {
             when(val result = getSearchs(query = query)){
-                is NetworkResult.Error -> Log.d("SearchViewModel", "error Search")
-                is NetworkResult.Exception -> Log.d("SearchViewModel", "exception Search")
-                is NetworkResult.Success -> _search.value = result.data.results.map {
-                    buildPosterUrlUseCase(it.posterPath)?: ""
+                is NetworkResult.Error -> UiState.Error(result.message.toString())
+                is NetworkResult.Exception -> UiState.Error(result.e.message.toString())
+                is NetworkResult.Success -> {
+                    _search.value = UiState.Success(
+                        result.data.results.map {
+                            buildPosterUrlUseCase(it.posterPath) ?: ""
+                        }
+                    )
                 }
             }
         }
     }
+}
+sealed class UiState {
+    data class Success(val items: List<String>) : UiState()
+    object Loading : UiState()
+    data class Error(val msg: String) : UiState()
 }
